bd56204696c263a061df4afcccdc12b571bc923d
root <root@ip-172-31-33-238.eu-west-1.compute.internal>
2020-11-18 08:13:55 +0000

    initial push

---
+++ src/main/java/com/github/App.java
@@ -0,0 +1,17 @@
+package com.github;
+
+/**
+ * Hello again
+ * Hello world!
+ * Hello
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	//Comment
+        System.out.println( "Hello World!" );
+    }
+}
+
+//foo
